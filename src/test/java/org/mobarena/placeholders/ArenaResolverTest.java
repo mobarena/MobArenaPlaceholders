2a8026b90b17afc98e2290a6daecdcf37713d2bd
Andreas Troelsen <garbagemule@gmail.com>
2022-06-19 23:48:52 +0200

    Remove display name of nested placeholders class.
    Not really necessary and doesn't really provide much here.

--- src/test/java/org/mobarena/placeholders/ArenaResolverTest.java
+++ src/test/java/org/mobarena/placeholders/ArenaResolverTest.java
@@ -10,7 +10,6 @@ import org.bukkit.configuration.MemoryConfiguration;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -117,7 +116,6 @@ class ArenaResolverTest {
     }
 
     @Nested
-    @DisplayName("Arena placeholders")
     class Placeholders {
 
         OfflinePlayer target;
