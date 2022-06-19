c78d866e8b7414a2e5a5bfaa3ace30805ff7127a
Andreas Troelsen <garbagemule@gmail.com>
2022-06-19 20:54:40 +0200

    Add test suite for arena placeholders.
    This pretty much completes the test suite for the ArenaResolver. There
    could be edge cases in the arena behavior that makes this incomplete or
    even wrong, but at least the specification and intent is there.

--- src/test/java/org/mobarena/placeholders/ArenaResolverTest.java
+++ src/test/java/org/mobarena/placeholders/ArenaResolverTest.java
@@ -1,11 +1,17 @@
 package org.mobarena.placeholders;
 
+import com.garbagemule.MobArena.MonsterManager;
 import com.garbagemule.MobArena.framework.Arena;
+import com.garbagemule.MobArena.util.timer.AutoStartTimer;
+import com.garbagemule.MobArena.waves.WaveManager;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.MemoryConfiguration;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -108,6 +114,261 @@ class ArenaResolverTest {
 
         String expected = String.valueOf(players.size());
         assertThat(result, equalTo(expected));
+    }
+
+    @Nested
+    @DisplayName("Arena placeholders")
+    class Placeholders {
+
+        OfflinePlayer target;
+        String slug = "castle";
+        Arena arena;
+
+        @BeforeEach
+        void setup() {
+            target = null;
+            arena = mock(Arena.class);
+            when(lookup.lookup(target, slug)).thenReturn(arena);
+        }
+
+        @Test
+        void slug() {
+            when(arena.getSlug()).thenReturn(slug);
+
+            String result = subject.resolve(target, slug + "_slug");
+
+            assertThat(result, equalTo(slug));
+        }
+
+        @Test
+        void stateEditing() {
+            String expected = "In Edit Mode";
+            config.set("editing", expected);
+            when(arena.inEditMode()).thenReturn(true);
+
+            String result = subject.resolve(target, slug + "_state");
+
+            assertThat(result, equalTo(expected));
+        }
+
+        @Test
+        void stateRunning() {
+            String expected = "Running";
+            config.set("running", expected);
+            when(arena.inEditMode()).thenReturn(false);
+            when(arena.isRunning()).thenReturn(true);
+
+            String result = subject.resolve(target, slug + "_state");
+
+            assertThat(result, equalTo(expected));
+        }
+
+        @Test
+        void stateAvailable() {
+            String expected = "Open For Business";
+            config.set("available", expected);
+            when(arena.inEditMode()).thenReturn(false);
+            when(arena.isRunning()).thenReturn(false);
+            when(arena.isEnabled()).thenReturn(true);
+
+            String result = subject.resolve(target, slug + "_state");
+
+            assertThat(result, equalTo(expected));
+        }
+
+        @Test
+        void stateDisabled() {
+            String expected = "Under Construction";
+            config.set("disabled", expected);
+            when(arena.inEditMode()).thenReturn(false);
+            when(arena.isRunning()).thenReturn(false);
+            when(arena.isEnabled()).thenReturn(false);
+
+            String result = subject.resolve(target, slug + "_state");
+
+            assertThat(result, equalTo(expected));
+        }
+
+        @Test
+        void minPlayers() {
+            int threshold = 2;
+            when(arena.getMinPlayers()).thenReturn(threshold);
+
+            String result = subject.resolve(target, slug + "_min-players");
+
+            String expected = String.valueOf(threshold);
+            assertThat(result, equalTo(expected));
+        }
+
+        @Test
+        void maxPlayers() {
+            int threshold = 3;
+            when(arena.getMaxPlayers()).thenReturn(threshold);
+
+            String result = subject.resolve(target, slug + "_max-players");
+
+            String expected = String.valueOf(threshold);
+            assertThat(result, equalTo(expected));
+        }
+
+        @Test
+        void autoStartTimerInSeconds() {
+            long ticks = 85;
+            int secs = (int) (ticks / 20);
+            AutoStartTimer timer = mock(AutoStartTimer.class);
+            when(arena.getAutoStartTimer()).thenReturn(timer);
+            when(timer.getRemaining()).thenReturn(ticks);
+
+            String result = subject.resolve(null, slug + "_auto-start-timer");
+
+            String expected = String.valueOf(secs);
+            assertThat(result, equalTo(expected));
+        }
+
+        @Test
+        void currentWave() {
+            int wave = 13;
+            WaveManager waves = mock(WaveManager.class);
+            when(arena.getWaveManager()).thenReturn(waves);
+            when(waves.getWaveNumber()).thenReturn(wave);
+
+            String result = subject.resolve(target, slug + "_current-wave");
+
+            String expected = String.valueOf(wave);
+            assertThat(result, equalTo(expected));
+        }
+
+        @Test
+        void finalWaveNonZero() {
+            int wave = 30;
+            WaveManager waves = mock(WaveManager.class);
+            when(arena.getWaveManager()).thenReturn(waves);
+            when(waves.getFinalWave()).thenReturn(wave);
+
+            String result = subject.resolve(target, slug + "_final-wave");
+
+            String expected = String.valueOf(wave);
+            assertThat(result, equalTo(expected));
+        }
+
+        @Test
+        void finalWaveInfinity() {
+            WaveManager waves = mock(WaveManager.class);
+            when(arena.getWaveManager()).thenReturn(waves);
+            when(waves.getFinalWave()).thenReturn(0);
+
+            String result = subject.resolve(target, slug + "_final-wave");
+
+            assertThat(result, equalTo("∞"));
+        }
+
+        @Test
+        void lobbyPlayers() {
+            Set<Player> players = Set.of(
+                mock(Player.class),
+                mock(Player.class),
+                mock(Player.class)
+            );
+            when(arena.getPlayersInLobby()).thenReturn(players);
+
+            String result = subject.resolve(target, slug + "_lobby-players");
+
+            String expected = String.valueOf(players.size());
+            assertThat(result, equalTo(expected));
+        }
+
+        @Test
+        void readyPlayers() {
+            Set<Player> players = Set.of(
+                mock(Player.class)
+            );
+            when(arena.getReadyPlayersInLobby()).thenReturn(players);
+
+            String result = subject.resolve(target, slug + "_ready-players");
+
+            String expected = String.valueOf(players.size());
+            assertThat(result, equalTo(expected));
+        }
+
+        @Test
+        void livePlayers() {
+            Set<Player> players = Set.of(
+                mock(Player.class),
+                mock(Player.class)
+            );
+            when(arena.getPlayersInArena()).thenReturn(players);
+
+            String result = subject.resolve(target, slug + "_live-players");
+
+            String expected = String.valueOf(players.size());
+            assertThat(result, equalTo(expected));
+        }
+
+        @Test
+        void deadPlayersNotRunning() {
+            when(arena.isRunning()).thenReturn(false);
+
+            String result = subject.resolve(target, slug + "_dead-players");
+
+            assertThat(result, equalTo("0"));
+        }
+
+        @Test
+        void deadPlayersRunning() {
+            int initial = 15;
+            Set<Player> players = Set.of(
+                mock(Player.class),
+                mock(Player.class)
+            );
+            when(arena.isRunning()).thenReturn(true);
+            when(arena.getPlayerCount()).thenReturn(initial);
+            when(arena.getPlayersInArena()).thenReturn(players);
+
+            String result = subject.resolve(target, slug + "_dead-players");
+
+            String expected = String.valueOf(initial - players.size());
+            assertThat(result, equalTo(expected));
+        }
+
+        @Test
+        void initialPlayersNotRunning() {
+            when(arena.isRunning()).thenReturn(false);
+
+            String result = subject.resolve(target, slug + "_initial-players");
+
+            assertThat(result, equalTo("0"));
+        }
+
+        @Test
+        void initialPlayersRunning() {
+            int initial = 15;
+            when(arena.isRunning()).thenReturn(true);
+            when(arena.getPlayerCount()).thenReturn(initial);
+
+            String result = subject.resolve(target, slug + "_initial-players");
+
+            String expected = String.valueOf(initial);
+            assertThat(result, equalTo(expected));
+        }
+
+        @Test
+        void liveMobs() {
+            Set<LivingEntity> monsters = Set.of(
+                mock(LivingEntity.class),
+                mock(LivingEntity.class),
+                mock(LivingEntity.class),
+                mock(LivingEntity.class)
+            );
+            MonsterManager manager = mock(MonsterManager.class);
+            when(arena.getMonsterManager()).thenReturn(manager);
+            when(manager.getMonsters()).thenReturn(monsters);
+
+            String result = subject.resolve(target, slug + "_live-mobs");
+
+            String expected = String.valueOf(monsters.size());
+            assertThat(result, equalTo(expected));
+        }
+
     }
 
 }
